8042258af2351aec7c25e4a8843b4520af88ab92
afonso <afonso@yack.com.br>
2015-10-14 00:19:09 -0300

    include the format param usage example: /search?q=cat&f=txt or /search?q=cat&f=json json format is default if 'f' is empty

--- examples/http.go
+++ examples/http.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/garoago/runelib"
 	"html"
@@ -10,35 +11,68 @@ import (
 )
 
 func main() {
-	//http.Handle("/foo", fooHandler)
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hellow, %q", html.EscapeString(r.URL.Path))
 	})
 
 	http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
 		search := r.URL.Query()
+		format := search.Get("f")
 		words := strings.Split(search.Get("q"), " ")
-		saved := make(chan bool)
-		runeIndex := runelib.GetIndex(saved)
-		count := 0
-		format := "U+%04X  %c \t%s\n"
 
-		for _, uchar := range runeIndex.Find(words) {
-			if uchar > 0xFFFF {
-				format = "U+%5X %c \t%s\n"
-			}
-			fmt.Fprintf(w, format, uchar, uchar, runeIndex.Name(uchar))
-			count++
+		if format == "" {
+			format = "json"
 		}
 
-		fmt.Fprintf(w, "%d characters found\n", count)
-		if <-saved {
-			fmt.Fprint(w, "Index saved.")
+		if format == "txt" {
+			txtSearch(w, words)
+		} else if format == "json" {
+			jsonSearch(w, words)
 		}
-
-		fmt.Fprintf(w, "busca, %q", search)
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
+}
+
+type SearchResponse struct {
+	Count  int
+	Result runelib.RuneSlice
+}
+
+func jsonSearch(w http.ResponseWriter, words []string) {
+	saved := make(chan bool)
+	runeIndex := runelib.GetIndex(saved)
+
+	result := runeIndex.Find(words)
+	count := len(result)
+
+	output := SearchResponse{
+		Count:  count,
+		Result: result,
+	}
+
+	serialized, _ := json.Marshal(output)
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, string(serialized))
+}
+
+func txtSearch(w http.ResponseWriter, words []string) {
+	saved := make(chan bool)
+	runeIndex := runelib.GetIndex(saved)
+	count := 0
+	template := "U+%04X  %c \t%s\n"
+
+	for _, uchar := range runeIndex.Find(words) {
+		if uchar > 0xFFFF {
+			template = "U+%5X %c \t%s\n"
+		}
+		fmt.Fprintf(w, template, uchar, uchar, runeIndex.Name(uchar))
+		count++
+	}
+
+	fmt.Fprintf(w, "%d characters found\n", count)
+	if <-saved {
+		fmt.Fprint(w, "Index saved.")
+	}
 }
