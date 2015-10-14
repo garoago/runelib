bf1aa41865b614987eaa3be4809e25ac210e0284
afonso <afonso@yack.com.br>
2015-10-13 22:22:54 -0300

    create runelib example initial http usage usage example: /search/cat or /search/cat face response not in json yet

---
+++ examples/http.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"github.com/garoago/runelib"
+	"html"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func main() {
+	//http.Handle("/foo", fooHandler)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hellow, %q", html.EscapeString(r.URL.Path))
+	})
+
+	http.HandleFunc("/search/", func(w http.ResponseWriter, r *http.Request) {
+		search := strings.SplitAfter(html.EscapeString(r.URL.Path), "/search/")[1]
+		words := strings.Split(search, " ")
+		saved := make(chan bool)
+		runeIndex := runelib.GetIndex(saved)
+		count := 0
+		format := "U+%04X  %c \t%s\n"
+
+		for _, uchar := range runeIndex.Find(words) {
+			if uchar > 0xFFFF {
+				format = "U+%5X %c \t%s\n"
+			}
+			fmt.Fprintf(w, format, uchar, uchar, runeIndex.Name(uchar))
+			count++
+		}
+
+		fmt.Fprintf(w, "%d characters found\n", count)
+		if <-saved {
+			fmt.Fprint(w, "Index saved.")
+		}
+
+		fmt.Fprintf(w, "busca, %q", search)
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+}
